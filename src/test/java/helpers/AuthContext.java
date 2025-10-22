b7ed22239b9888d0beb04cea08b47ecbeb2a5dbb
sergejgluhov <arane1223@gmail.com>
2025-10-22 12:10:59 +0500

    deleted expires in AuthContext

--- src/test/java/helpers/AuthContext.java
+++ src/test/java/helpers/AuthContext.java
@@ -4,12 +4,10 @@ public class AuthContext {
 
     private static final ThreadLocal<String> TOKEN = new ThreadLocal<>();
     private static final ThreadLocal<String> USER_ID = new ThreadLocal<>();
-    private static final ThreadLocal<String> EXPIRES = new ThreadLocal<>();
 
     public static void set(String token, String userId, String expires) {
         TOKEN.set(token);
         USER_ID.set(userId);
-        EXPIRES.set(expires);
     }
 
     public static String getToken() {
@@ -20,13 +18,9 @@ public class AuthContext {
         return USER_ID.get();
     }
 
-    public static String getExpires() {
-        return EXPIRES.get();
-    }
 
     public static void clear() {
         TOKEN.remove();
         USER_ID.remove();
-        EXPIRES.remove();
     }
 }
